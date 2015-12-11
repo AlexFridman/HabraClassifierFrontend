acb641ce34aff78b78f529b0e1b2f381a40b1d81
Егор <egor_metlitsky@mail.ru>
2015-12-11 14:51:00 +0300

    Added urls

--- HabraClassifierFrontend/urls.py
+++ HabraClassifierFrontend/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
+from app import views
 # from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url(r'^$', views.TopicClassificationView.as_view(), name="homepage"),
+    url(r'^topic/$', views.TopicClassificationView.as_view(), name="topic_classification"),
+    url(r'^text/$', views.TextClassificationView.as_view(), name="text_classification"),
 ]
